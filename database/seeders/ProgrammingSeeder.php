9f98e3e55a7ffce8a63fb7a7525ad3b50dc812f0
Julio Jimenez <zaor.julio@gmail.com>
2023-10-17 22:08:47 -0500

    ajuste de seeder

--- database/seeders/ProgrammingSeeder.php
+++ database/seeders/ProgrammingSeeder.php
@@ -17,6 +17,8 @@ class ProgrammingSeeder extends Seeder
             'id'=>1,
             'quota' => 999999999,
             'quota_available' => 999999999,
+            'initial_date'=> '2025-12-01',
+            'initial_time' => '10:00:00',
             'event_id' => 1,
             'waiting' => true
         ]);
