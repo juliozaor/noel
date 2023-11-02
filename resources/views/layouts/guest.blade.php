6de15af7982427e47f1ccad78262755d74b195ce
Cristian Velasquez <cristian.velasquez@dation.co>
2023-11-01 20:12:36 -0500

    Ajuste de Favicon para prueba de estado 200 desde aws

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" >
         <link rel="stylesheet" href="{{ asset('/assets/css/autentication.css') }}">
         <!-- Fonts -->
         <link
