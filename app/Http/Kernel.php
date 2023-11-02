f67fcbcddd760546c9759dda835329c18b398d9a
Cristian Velasquez <cristian.velasquez@dation.co>
2023-11-01 21:30:05 -0500

    Ajuste de control de peticiones por minuto

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api,300,1',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
