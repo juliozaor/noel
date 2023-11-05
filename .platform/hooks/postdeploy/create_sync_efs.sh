d0788d26c6393dbf0db2613117d59ea7a468a239
Cristian Velasquez <cristian.velasquez@dation.co>
2023-11-05 00:50:11 -0500

    Ajuste de Configuracion de ambiente AWS ELB + EFS

---
+++ .platform/hooks/postdeploy/create_sync_efs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SYMLINK="/var/app/current/public/temp"
+TARGET="/efs/temp"
+
+if [ -L "$SYMLINK" ]; then
+    if [ "$(readlink "$SYMLINK")" != "$TARGET" ]; then
+        echo "Eliminando el enlace simbólico existente en $SYMLINK"
+        sudo rm -R "$SYMLINK"
+    else
+        echo "El enlace simbólico ya apunta a $TARGET."
+        exit 1
+    fi
+fi
+
+if [ -d "$SYMLINK" ]; then
+    echo "Eliminando el directorio existente en $SYMLINK"
+    sudo rm -R "$SYMLINK"
+fi
+
+echo "Creando nuevo enlace simbólico en $SYMLINK apuntando a $TARGET"
+sudo ln -s "$TARGET" /var/app/current/public
+   
+sudo chmod 775 /var/app/current/public/ -R
+sudo chmod 775 /efs -R
